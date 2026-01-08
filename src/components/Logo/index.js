0ea945d4fac197905b5441d906501a406b8f9e5f
Druzian <druziii@hotmail.com>
2026-01-08 06:20:24 -0300

    Criação da branch dev e logo adicionada

---
+++ src/components/Logo/index.js
@@ -0,0 +1,7 @@
+import Styles from './logo.module.css';
+
+export default function Logo() {
+  return (
+    <img className={Styles.logo} src="/assets/images/dente.png" alt="Logo" />
+  );
+}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,19 @@
 import Styles from './navbar.module.css';
 import Navitem from '../Navitem';
+import Logo from '../Logo';
 
 export default function Navbar() {
   return (
     <nav className={Styles.navbar}>
-      <ul className={Styles.navList}>
-        <Navitem label="Agende sua avaliação" link="#aval" />
-        <Navitem label="Tratamentos" link="#tratamentos" />
-        <Navitem label="Clínicas" link="#clinicas" />
-        <Navitem label="Sobre nós" link="#sobre" />
-      </ul>
+      <div className={Styles.navContainer}>
+      <Logo />
+        <ul className={Styles.navList}>
+          <Navitem label="Agende sua avaliação" link="#aval" />
+          <Navitem label="Tratamentos" link="#tratamentos" />
+          <Navitem label="Clínicas" link="#clinicas" />
+          <Navitem label="Sobre nós" link="#sobre" />
+        </ul>
+      </div>
     </nav>
   );
 }
